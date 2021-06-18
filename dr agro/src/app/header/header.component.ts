2e51bdf1312192ed159e562b28b29fc7ff719b60
Ettilla <1610058@student.ruet.ac.bd>
2021-06-18 20:57:30 +0600

    Saving token in the local storage

--- dr agro/src/app/header/header.component.ts
+++ dr agro/src/app/header/header.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
-
-
 import { Subscription } from "rxjs";
 
 import { AuthService } from "../auth/auth.service";
@@ -15,9 +13,9 @@ export class HeaderComponent implements OnInit, OnDestroy {
   private authListenerSubs: Subscription;
 
   constructor(private authService: AuthService) {}
-  
 
   ngOnInit() {
+    this.userIsAuthenticated = this.authService.getIsAuth();
     this.authListenerSubs = this.authService
       .getAuthStatusListener()
       .subscribe(isAuthenticated => {
